72c0ba823b1293d887bf7677b0cec3f5dc1d9c47
pallat <yod.pallat@gmail.com>
2018-08-21 21:23:25 +0700

    wrap gin.Context and gin.HandlerFunc

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/gin-gonic/gin v1.3.0 // indirect
 	github.com/golang/protobuf v1.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/ugorji/go v1.1.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.2.1 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", middleware, handler)
+	r.GET("/ping", Compose(handler, uuid.NewV4().String()))
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func handler(c *Context) {
+	c.JSON(200, gin.H{
+		"id": c.UUID(),
+	})
+}
+
+func Compose(h HandlerFunc, id string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer = &Writer{ResponseWriter: c.Writer, id: id}
+		c.Set("id", id)
+
+		ctx := &Context{c}
+
+		log.Println(id)
+		h(ctx)
+	}
+}
+
+type HandlerFunc func(*Context)
+type Context struct {
+	*gin.Context
+}
+
+func (c *Context) UUID() string {
+	if uid, ok := c.Get("id"); ok {
+		return uid.(string)
+	}
+	return ""
+}
+
+type Writer struct {
+	id string
+	gin.ResponseWriter
+}
+
+func (w *Writer) Write(b []byte) (int, error) {
+	defer log.Println(w.id, string(b))
+	return w.ResponseWriter.Write(b)
 }
